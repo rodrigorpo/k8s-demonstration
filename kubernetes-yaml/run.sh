bead2bc62f30b257178c52dc1451d3c4268bafbb
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-05-27 11:56:50 -0300

    K8s start

--- kubernetes-yaml/run.sh
+++ kubernetes-yaml/run.sh
@@ -1,7 +1,8 @@
 minikube start --nodes 3 --mount-string="$MINIKUBE_HOME/files/etc:/data"
-k taint nodes minikube reason=master_node:NoSchedule
+kubectl taint nodes minikube reason=master_node:NoSchedule
 
 minikube addons enable dashboard
 minikube addons enable ingress
 
-k apply -f metric-server-for-minikube
+kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml
+kubectl apply -f files/metric-server-for-minikube
